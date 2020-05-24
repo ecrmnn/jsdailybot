6050342f63f4ae6c43659f8ef6d5c453e01f1076
ecrmnn <danieleckermann@gmail.com>
2020-05-24 13:49:29 +0200

    Adds handler function and updates serverless.yml

--- src/index.js
+++ src/index.js
@@ -2,26 +2,35 @@ const Twitter = require('twitter');
 const { readFileSync } = require('fs');
 const config = require('./config.js');
 
-const client = new Twitter(config);
+module.exports.handler = async (event, context, callback) => {
+  const client = new Twitter(config);
 
-// Upload photo
-const photo = readFileSync('./snippets/img/copy.js.png');
+  // Upload photo
+  const photo = readFileSync('./snippets/img/copy.js.png');
 
-client.post('media/upload', { media: photo }, (error, media) => {
-  if (!error) {
-    const status = {
-      status: 'I am a tweet',
-      media_ids: media.media_id_string,
-    };
+  client.post('media/upload', { media: photo }, (error, media) => {
+    if (!error) {
+      const status = {
+        status: 'I am a tweet',
+        media_ids: media.media_id_string,
+      };
 
-    client.post('statuses/update', status, (tweetError, tweet) => {
-      if (!tweetError) {
+      client.post('statuses/update', status, (tweetError, tweet) => {
+        if (!tweetError) {
         // eslint-disable-next-line
         console.log(tweet);
-      }
+        }
+
+        return callback(null, {
+          statusCode: 200,
+          body: 'html',
+        });
+      });
+    }
+
+    return callback(null, {
+      statusCode: 200,
+      body: 'html',
     });
-  } else {
-    // eslint-disable-next-line
-    console.log(error);
-  }
-});
+  });
+};
