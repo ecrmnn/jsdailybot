b0e8040956ee6cfcbe091e5f187ec1c74f213dad
ecrmnn <danieleckermann@gmail.com>
2020-05-24 13:26:51 +0200

    Removes APP_ENV

--- bin/deployment.sh
+++ bin/deployment.sh
@@ -11,12 +11,6 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "Setting Node.js runtime environment"
   echo ""
 
-  sed -i "s|local|$APP_STAGE|g" $TRAVIS_BUILD_DIR/serverless.yml
-
-  echo ""
-  echo "Deploying to AWS Lambda"
-  echo ""
-
   npm install serverless --global
 
   serverless deploy --stage $APP_STAGE
