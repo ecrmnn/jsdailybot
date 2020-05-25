4ee9d78a665a06e0c64ab7755b47ff76d9c094f8
ecrmnn <danieleckermann@gmail.com>
2020-05-25 19:08:10 +0200

    Removes tests

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,14 @@
-const { describe, it } = require('mocha');
-const { expect } = require('chai');
-const { readdirSync } = require('fs');
+// const { describe, it } = require('mocha');
+// const { expect } = require('chai');
+// const { readdirSync } = require('fs');
 
-describe('Test Suite', () => {
-  it('should have generated images for every snippet', () => {
-    const codeSnippets = readdirSync('./snippets/code/');
-    const imageSnippets = readdirSync('./snippets/img/')
-      .filter((filename) => filename !== '.gitkeep')
-      .map((filename) => filename.substring(0, filename.length - 4));
+// describe('Test Suite', () => {
+//   it('should have generated images for every snippet', () => {
+//     const codeSnippets = readdirSync('./snippets/code/');
+//     const imageSnippets = readdirSync('./snippets/img/')
+//       .filter((filename) => filename !== '.gitkeep')
+//       .map((filename) => filename.substring(0, filename.length - 4));
 
-    expect(codeSnippets).to.eql(imageSnippets);
-  });
-});
+//     expect(codeSnippets).to.eql(imageSnippets);
+//   });
+// });
