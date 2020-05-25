7e4dff88088a10d9d6ff49b726a3cb786e0cffd2
ecrmnn <danieleckermann@gmail.com>
2020-05-25 20:01:27 +0200

    Improves tests

--- tests/test.js
+++ tests/test.js
@@ -4,10 +4,13 @@ const { generateImage, getRandomSnippet } = require('../src');
 
 describe('Test Suite', () => {
   it('should pick a random snippet to tweet', () => {
-    const first = getRandomSnippet();
-    const second = getRandomSnippet();
+    const set = new Set([
+      getRandomSnippet().body,
+      getRandomSnippet().body,
+      getRandomSnippet().body,
+    ]);
 
-    expect(first.body).not.to.eql(second.body);
+    expect(set.size).to.be.greaterThan(1);
   });
 
   it('should generate a code screenshot of the given text', async () => {
